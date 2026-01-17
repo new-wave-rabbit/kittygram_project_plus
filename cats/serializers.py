68452d9eff5155b85048509eac01c568da41c5d0
dotsenkois <dotsenkois@yandex.ru>
2026-01-17 18:59:28 +0700

    awd

--- cats/serializers.py
+++ cats/serializers.py
@@ -10,7 +10,7 @@ class AchievementSerializer(serializers.ModelSerializer):
         fields = ('id', 'name')
 
 class CatSerializer(serializers.ModelSerializer):
-    achievements = AchievementSerializer(many=True)  # Убрали read_only=True
+    achievements = AchievementSerializer(many=True, required=False)
     # Убрали owner = serializers.StringRelatedField(read_only=True)
     
     class Meta:
@@ -18,22 +18,25 @@ class CatSerializer(serializers.ModelSerializer):
         fields = ('id', 'name', 'color', 'birth_year', 'owner', 'achievements')
 
     def create(self, validated_data):
+        # Если в исходном запросе не было поля achievements
+        if 'achievements' not in self.initial_data:
+            # То создаём запись о котике без его достижений
+            cat = Cat.objects.create(**validated_data)
+            return cat
+
+        # Иначе делаем следующее:
         # Уберём список достижений из словаря validated_data и сохраним его
         achievements = validated_data.pop('achievements')
-
-        # Создадим нового котика пока без достижений, данных нам достаточно
+        # Сначала добавляем котика в БД
         cat = Cat.objects.create(**validated_data)
-
-        # Для каждого достижения из списка достижений
+        # А потом добавляем его достижения в БД
         for achievement in achievements:
-            # Создадим новую запись или получим существующий экземпляр из БД
             current_achievement, status = Achievement.objects.get_or_create(
                 **achievement)
-            # Поместим ссылку на каждое достижение во вспомогательную таблицу
-            # Не забыв указать к какому котику оно относится
+            # И связываем каждое достижение с этим котиком
             AchievementCat.objects.create(
                 achievement=current_achievement, cat=cat)
-        return cat
+        return cat 
 
 
 
