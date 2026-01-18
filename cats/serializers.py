8954b8fd67d88b0e9480b782dfc8896ccae599f0
idotsenko <idotsenko@usergate.com>
2026-01-18 08:14:47 +0300

    Добавлен класс Hex2NameColor

--- cats/serializers.py
+++ cats/serializers.py
@@ -1,10 +1,28 @@
 # cats/serializers.py
 import datetime as dt
+import webcolors
 
 from rest_framework import serializers
 
 from .models import Cat, Owner, Achievement, AchievementCat
 
+
+class Hex2NameColor(serializers.Field):
+    # При чтении данных ничего не меняем - просто возвращаем как есть
+    def to_representation(self, value):
+        return value
+
+    # При записи код цвета конвертируется в его название
+    def to_internal_value(self, data):
+        # Доверяй, но проверяй
+        try:
+            # Если имя цвета существует, то конвертируем код в название
+            data = webcolors.hex_to_name(data)
+        except ValueError:
+            # Иначе возвращаем ошибку
+            raise serializers.ValidationError('Для этого цвета нет имени')
+        # Возвращаем данные в новом формате
+        return data
 
 
 class AchievementSerializer(serializers.ModelSerializer):
@@ -12,6 +30,7 @@ class AchievementSerializer(serializers.ModelSerializer):
     class Meta:
         model = Achievement
         fields = ('id', 'name')
+
 
 class CatSerializer(serializers.ModelSerializer):
     achievements = AchievementSerializer(many=True, required=False)
